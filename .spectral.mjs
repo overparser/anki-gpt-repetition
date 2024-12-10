01c6835bf9ce1e5c990875d52489113cdc4b6653
overparser <derror.dd@gmail.com>
2024-12-10 22:01:20 +0500

    Modified 3 files

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2Njc1NA/spectral.js?branch_slug=main&token=2a9a002f-1812-4286-bf49-f833954606f5";
-export default { extends: ruleset };
